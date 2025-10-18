4fa5bc64b631a5637cfef76ec7b3e6831111d075
Bala Kumar <balaashok2004@gmail.com>
2025-10-18 20:56:38 +0530

    Added repository layer and chaned int -> long

--- src/main/java/com/bala/Todo/model/TodoModel.java
+++ src/main/java/com/bala/Todo/model/TodoModel.java
@@ -11,7 +11,7 @@ public class TodoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     @Column(name = "task_name" , nullable = false , unique = true)
     private String name;
     @Column(name = "task_description" , nullable = false)
@@ -28,11 +28,11 @@ public class TodoModel {
         this.description=description;
         this.taskCompleted=taskCompleted;
     }
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
---
+++ src/main/java/com/bala/Todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.bala.Todo.repository;
+
+import com.bala.Todo.model.TodoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoModel , Long> {
+}
